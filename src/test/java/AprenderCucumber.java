8b4fa5d6642d71370f80fb6d47f7d4141ac6a56a
George Salu <george.salu10@gmail.com>
2019-02-09 13:59:31 -0200

    cucumberOptions

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,6 +1,6 @@
 import cucumber.api.PendingException;
 import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
@@ -17,9 +17,9 @@ public class AprenderCucumber {
 		throw new PendingException();
 	}
 
-	@Entao("^a espeficiacao deve finalizar com sucesso")
-	public void a_especificacao_com_sucesso() throws Throwable {
-		// write code here that the phrase above into concrete actions
+	@Então("^a especificação deve finaliazr com sucesso$")
+	public void aEspecificaçãoDeveFinaliazrComSucesso() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
 		throw new PendingException();
 	}
 
